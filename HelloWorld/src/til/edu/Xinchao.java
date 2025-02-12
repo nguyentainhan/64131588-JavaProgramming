8ff195406454863df751c3988cd14db4ab59355b
nguyentainhan <ntnhan852017@gmail.com>
2025-02-12 21:27:49 +0700

    Ví dụ 1
    Demo với github

---
+++ HelloWorld/src/til/edu/Xinchao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package til.edu;
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("XIn chào tôi là Nguyễn Tài Nhân khóa 64 TTMMT!");
+	}
+
+}
