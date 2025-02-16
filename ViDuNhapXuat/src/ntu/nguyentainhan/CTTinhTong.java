e3f0dbfe44de708cc42d40593549d8f4d47cb446
nguyentainhan <ntnhan852017@gmail.com>
2025-02-16 21:12:35 +0700

    full code

--- ViDuNhapXuat/src/ntu/nguyentainhan/CTTinhTong.java
+++ ViDuNhapXuat/src/ntu/nguyentainhan/CTTinhTong.java
@@ -1,10 +1,24 @@
 package ntu.nguyentainhan;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//In ra 1 dòng tiêu đề app
+		System.out.print("CT TINH TONG \n");
+		//Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		//In dong huong dan moi nhap A
+		System.out.println("Mời nhập a: ");
+		double a = banPhim.nextDouble();
+		System.out.println("Mời nhập b: ");
+		double b = banPhim.nextDouble();
+		//Tính toán
+		double tong = a + b;
+		//Xuất
+		System.out.print("Tổng là: ");
+		System.out.print(tong);
 	}
 
 }
